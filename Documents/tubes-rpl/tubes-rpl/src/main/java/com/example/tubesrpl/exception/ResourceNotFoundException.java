aecfa5f2a2864d63c9de2b82f70b6bc1ad87d2ab
AzrielAldrinSenandoSiahaan <6182201070@student.unpar.ac.id>
2025-12-14 11:17:37 +0700

    Hapus file yang tidak dibutuhkan

--- Documents/tubes-rpl/tubes-rpl/src/main/java/com/example/tubesrpl/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.tubesrpl.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
